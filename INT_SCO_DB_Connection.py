07320cdc64364e2de7f32137ee79de3b8e29c375
jayson wang <jwang8@frdm.ca>
2024-02-27 12:15:13 -0500

    modufied 20240227

--- INT_SCO_DB_Connection.py
+++ INT_SCO_DB_Connection.py
@@ -5,8 +5,15 @@ from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives.asymmetric import dsa
 from cryptography.hazmat.primitives import serialization
 
+user = os.environ.get('SF_Connection_proc_acct')
+#print(user)
+
+key_path = os.environ.get('SF_Connection_PRIVATE_KEY_PATH')
+#print(path)
+
+
 def get_snowflake_conn():
-    with open("/home/jwang/10_200_52_21__rsa.p8", "rb") as key:
+    with open(key_path, "rb") as key:
         p_key= serialization.load_pem_private_key(
             key.read(),
             password=os.environ['SF_Connection_PRIVATE_KEY_PASSPHRASE'].encode(),
@@ -19,7 +26,7 @@ def get_snowflake_conn():
         encryption_algorithm=serialization.NoEncryption())
 
     ctx = snowflake.connector.connect(
-        user='proc_int_sco_national_retail_tospcetlapn01',
+        user=user,
         account='videotron-freedommobile',
         private_key=pkb,
         #warehouse="ANALYST_WH",
@@ -28,3 +35,35 @@ def get_snowflake_conn():
         
     )
     return ctx
+
+
+
+# with open(key_path, "rb") as key:
+    # p_key= serialization.load_pem_private_key(
+        # key.read(),
+        # password=os.environ['SF_Connection_PRIVATE_KEY_PASSPHRASE'].encode(),
+        # backend=default_backend()
+    # )
+
+# pkb = p_key.private_bytes(
+    # encoding=serialization.Encoding.DER,
+    # format=serialization.PrivateFormat.PKCS8,
+    # encryption_algorithm=serialization.NoEncryption())
+
+# ctx = snowflake.connector.connect(
+    # user=user,
+    # account='videotron-freedommobile',
+    # private_key=pkb,
+    # #warehouse="ANALYST_WH",
+    # database="INT_SCO_DB",
+    # schema="LANDING"
+    
+# )
+
+
+
+# # Use `ctx` as needed...
+# LND_cs = ctx.cursor()
+# LND_cs.execute("SELECT count(1) FROM INT_SCO_DB.LANDING.L0_WAREHOUSE_INVENTORY_EBS_DAILY;")
+# result = LND_cs.fetch_pandas_all()
+# print(result)
